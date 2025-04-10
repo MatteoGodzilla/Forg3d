65e7aaf45d6b00c95af882c19471d099c628a096
Matteo Catena <matteo.catena3@studio.unibo.it>
2025-04-10 15:13:32 +0200

    added style reference for various elements

---
+++ pages/styleReference.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/styleReference.css">
+    <title>Style Test</title>
+</head>
+<body>
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    <p>Paragrafo con del testo</p>
+    <a href="#">Link di prova</a>
+    <hr>
+    <button class="compratore">Primary compratore</button>
+    <button class="venditore">Primary venditore</button>
+    <button class="admin">Primary admin</button>
+    <button class="warning">Warning</button>
+    <button class="ok">Conferma</button>
+    <hr>
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email" />
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" />
+    <div>
+        <p>hlafslhfaslhfsadlhfaslh  </p>
+        <p>hlafslhfaslhfsadlhfaslh  </p>
+    </div>
+</body>
+</html>
