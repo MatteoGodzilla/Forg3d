920e313b00703a6240e1db6dac8feb90274887a7
Mattia Rocchi <mattia.rocchi2@studio.unibo.it>
2025-05-15 17:46:26 +0200

    Fix link in productList

--- pages/components/productList.php
+++ pages/components/productList.php
@@ -2,12 +2,13 @@
 <?php
 function generateProductList($product) {?>
 <article class="product">
-    <div>
-    <img src="https://placehold.co/800x600" alt="" />
-    <h3><?php  echo $product['nome'] ?> </h3>
-    <?php echo '<p>Venditore: ' . $product['venditoreNome'] . ' ' . $product['venditoreCognome'] . '</p>'?>
-        <?php echo '<a href="../product.php?id=' . $product['id'] . '">Dettagli</a>'?>
-    </div>
+    <a href="../product.php?id=<?php echo $product['id']; ?>">
+        <div>
+        <img src="https://placehold.co/800x600" alt="" />
+        <h3><?php  echo $product['nome'] ?> </h3>
+        <?php echo '<p>Venditore: ' . $product['venditoreNome'] . ' ' . $product['venditoreCognome'] . '</p>'?>
+        </div>
+    </a>
 </article>
 <?php
 }
