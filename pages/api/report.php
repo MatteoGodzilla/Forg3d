503957d5e77154aba88f102897f087ca31a0c45e
Mattia Rocchi <mattia.rocchi2@studio.unibo.it>
2025-05-26 21:19:16 +0200

    Create report api

---
+++ pages/api/report.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include_once("../../php/db.php");
+include_once("../../php/session.php");
+
+$emailUtente = getSessionEmail();
+$tipo = $_POST['tipo'];
+
+try{
+    mysqli_begin_transaction($connection);
+
+    $query ="INSERT INTO Segnalazione (emailSegnalatore, motivo) VALUES (?,?)";
+    $stmt = mysqli_prepare($connection,$query);
+    mysqli_stmt_bind_param($stmt, "ss", $emailUtente,$tipo);
+    mysqli_stmt_execute($stmt);
+
+    $idS = mysqli_insert_id($connection);
+
+    if ($tipo === 'venditore' && isset($_POST['emailVenditore'])) {
+        $emailVenditore = $_POST['emailVenditore'];
+        $qVenditore = "INSERT INTO SegnalazioneVenditore (idSegnalazione, emailVenditore) VALUES (?, ?)";
+        $stmtVenditore = mysqli_prepare($connection, $qVenditore);
+        mysqli_stmt_bind_param($stmtVenditore, "is", $idS, $emailVenditore);
+        mysqli_stmt_execute($stmtVenditore);
+    } elseif ($tipo === 'prodotto' && isset($_POST['idProdotto'])) {
+        $idProdotto = $_POST['idProdotto'];
+        $qProdotto = "INSERT INTO SegnalazioneProdotto (idSegnalazione, idProdotto) VALUES (?, ?)";
+        $stmtProdotto = mysqli_prepare($connection, $qProdotto);
+        mysqli_stmt_bind_param($stmtProdotto, "ii", $idS, $idProdotto);
+        mysqli_stmt_execute($stmtProdotto);
+    }
+    mysqli_commit($connection);
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+} catch(e){
+    mysqli_rollback($connection);
+}
