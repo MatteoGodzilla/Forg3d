5c626a6bb4fe7600d61c328d0bb972a22042db51
Matteo Catena <matteo.catena3@studio.unibo.it>
2025-05-26 11:57:24 +0200

    Added notification bell to header when logged in

--- pages/components/header.php
+++ pages/components/header.php
@@ -2,12 +2,14 @@
 <header>
 	<div class="header-content">
 		<a href="/"><h1>Forg3d</h1></a>
-			<?php if (utenteLoggato()){ ?>
-					<div id="userProfile"></div>
-					<a id="logout" href="./api/handleLogout.php">Logout</a>
-					<?php }else{ ?>
-						<a id="login" href="./login.php">Login</a>
-			<?php } ?>
+        <?php if (utenteLoggato()){ ?>
+            <div id="userProfile">
+                <span class="material-symbols-outlined">notifications</span>
+                <a id="logout" href="./api/handleLogout.php">Logout</a>
+            </div>
+        <?php } else { ?>
+            <a id="login" href="./login.php">Login</a>
+        <?php } ?>
 	</div>
 </header>
 <?php } ?>
