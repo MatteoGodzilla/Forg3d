374e4d42c88992533ebad67cd7a1c218c45731a4
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2025-06-10 17:43:12 +0200

    Fixed notifications again

--- pages/components/header.php
+++ pages/components/header.php
@@ -56,10 +56,9 @@
     $email = getSessionEmail();
     switch(getUserType()){
         case UserType::BUYER->value:
-            $query_notifiche = "SELECT COUNT(id) AS tot FROM Notifica WHERE (
-            emailMittente in (SELECT emailVenditore FROM Follow WHERE emailCompratore=?) OR emailMittente is NULL)
-            AND (emailDestinatario IS NULL OR emailDestinatario=?)
-            AND id NOT in (SELECT idNotifica FROM NotificaLetta WHERE destinatario=?)";
+            $query_notifiche = "SELECT COUNT(id) AS tot FROM Notifica WHERE 
+            ((emailMittente in (SELECT emailVenditore FROM Follow WHERE emailCompratore=?) AND emailDestinatario is NULL) OR emailMittente is NULL OR emailDestinatario = ?)  AND
+            id NOT in (SELECT idNotifica FROM NotificaLetta WHERE destinatario=?)";
             $stmt = mysqli_prepare($connection, $query_notifiche);
             mysqli_stmt_bind_param($stmt, "sss", $email,$email,$email);
             break;
