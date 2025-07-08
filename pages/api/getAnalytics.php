d3e1a61a7c9bf4b46600ed941ade87cbe1d3d981
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2025-07-08 11:22:22 -0400

    Fixed textual stats bug

--- pages/api/getAnalytics.php
+++ pages/api/getAnalytics.php
@@ -11,23 +11,23 @@
     $analytics = array();
 
     //check if a limit i specified
-    $time_constraint = "0";
+    $time_constraint = "TRUE";
     if(isset($_GET["limit"])){
         if($_GET["limit"]==="week"){
-            $time_constraint = " DATE_SUB(NOW(),INTERVAL 1 WEEK)";
+            $time_constraint = "MONTH(CURDATE())=MONTH(Ordine.dataCreazione) AND WEEK(CURDATE())=WEEK(InfoOrdine.dataCreazione)";
         }
         if($_GET["limit"]==="month"){
-            $time_constraint = " DATE_SUB(NOW(),INTERVAL 1 MONTH)";
+            $time_constraint = "MONTH(CURDATE())=MONTH(Ordine.dataCreazione)";
         }
         if($_GET["limit"]==="year"){
-            $time_constraint = " DATE_SUB(NOW(),INTERVAL 1 YEAR)";
+            $time_constraint = "YEAR(CURDATE())=YEAR(Ordine.dataCreazione)";
         }
     }
 
     //Buyer analytics: number of purchases,money spent,number of reports and of reviews made,number of sellers being followed
     if(getUserType() == UserType::BUYER->value){
         //purchases
-        $count_purchases = "SELECT COUNT(id) as Tot FROM Ordine WHERE emailCompratore = ? AND dataCreazione>=".$time_constraint;
+        $count_purchases = "SELECT COUNT(id) as Tot FROM Ordine WHERE emailCompratore = ? AND ".$time_constraint;
         $stmt = mysqli_prepare($connection, $count_purchases);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -37,7 +37,7 @@
 
         //money spent
         $money_spent = "SELECT CAST(SUM(prezzo*quantita)/100 AS DECIMAL(10,2)) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailCompratore = ? AND dataCreazione>=".$time_constraint;
+        WHERE emailCompratore = ? AND ".$time_constraint;
         $stmt = mysqli_prepare($connection, $money_spent);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -46,7 +46,7 @@
         $analytics["Denaro speso:"] = $row[0]["Tot"]."€";
 
         //reviews
-        $count_reviews = "SELECT COUNT(id) as Tot FROM Recensione WHERE email = ? AND dataCreazione>=".$time_constraint;
+        $count_reviews = "SELECT COUNT(id) as Tot FROM Recensione WHERE email = ? AND ".$time_constraint;
         $stmt = mysqli_prepare($connection, $count_reviews);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -76,7 +76,7 @@
 
         //Total revenue
         $money_earn = "SELECT COALESCE(CAST(SUM(prezzo*quantita)/100 AS DECIMAL(10,2)),0) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailVenditore = ? AND dataCreazione>=".$time_constraint;
+        WHERE emailVenditore = ? AND ".$time_constraint;
         $stmt = mysqli_prepare($connection, $money_earn);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -86,7 +86,7 @@
 
         //Total products sold
         $products_sell = "SELECT COALESCE(SUM(quantita),0) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailVenditore = ? AND dataCreazione>=".$time_constraint;
+        WHERE emailVenditore = ? AND ".$time_constraint;
         $stmt = mysqli_prepare($connection, $products_sell);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -97,7 +97,7 @@
         //Most sold product
         $most_sold = "SELECT COALESCE(SUM(quantita),0) as Tot,Prodotto.Nome as Nome,Prodotto.id AS pId FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
         INNER JOIN Variante ON InfoOrdine.idVariante = Variante.id INNER JOIN Prodotto ON Variante.idProdotto = Prodotto.id 
-        WHERE Prodotto.emailVenditore = ? AND dataCreazione>=".$time_constraint." GROUP BY pId ORDER BY Tot";
+        WHERE Prodotto.emailVenditore = ? AND ".$time_constraint." GROUP BY pId ORDER BY Tot";
         $stmt = mysqli_prepare($connection, $most_sold);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -113,7 +113,7 @@
         //Most revenue product
         $most_sold = "SELECT COALESCE(CAST(SUM(InfoOrdine.quantita*InfoOrdine.prezzo)/100 AS DECIMAL(10,2)),0) as Tot,Prodotto.Nome as Nome,Prodotto.id AS pId FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
         INNER JOIN Variante ON InfoOrdine.idVariante = Variante.id INNER JOIN Prodotto ON Variante.idProdotto = Prodotto.id 
-        WHERE Prodotto.emailVenditore = ? AND dataCreazione>=".$time_constraint." GROUP BY pId ORDER BY Tot";
+        WHERE Prodotto.emailVenditore = ? AND ".$time_constraint." GROUP BY pId ORDER BY Tot";
         $stmt = mysqli_prepare($connection, $most_sold);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
