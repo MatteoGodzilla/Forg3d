15d9d845d79dbe251fdf2f477c7874aaa0c88777
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2025-07-09 09:40:31 -0400

    Analytics have been semplified and are now consistent.

--- pages/api/getAnalytics.php
+++ pages/api/getAnalytics.php
@@ -11,23 +11,23 @@
     $analytics = array();
 
     //check if a limit i specified
-    $time_constraint = "TRUE";
+    $time_constraint = "652.985"; //to go below this,you would need to have assisted to the french revolution,i choose this number because is super goofy and it can be increased if necessary in the next 200 years
     if(isset($_GET["limit"])){
         if($_GET["limit"]==="week"){
-            $time_constraint = "MONTH(CURDATE())=MONTH(Ordine.dataCreazione) AND WEEK(CURDATE(),3)=WEEK(Ordine.dataCreazione,3)";
+            $time_constraint = "6";
         }
         if($_GET["limit"]==="month"){
-            $time_constraint = "MONTH(CURDATE())=MONTH(Ordine.dataCreazione)";
+            $time_constraint = "30";
         }
         if($_GET["limit"]==="year"){
-            $time_constraint = "YEAR(CURDATE())=YEAR(Ordine.dataCreazione)";
+            $time_constraint = "365";
         }
     }
 
     //Buyer analytics: number of purchases,money spent,number of reports and of reviews made,number of sellers being followed
     if(getUserType() == UserType::BUYER->value){
         //purchases
-        $count_purchases = "SELECT COUNT(id) as Tot FROM Ordine WHERE emailCompratore = ? AND ".$time_constraint;
+        $count_purchases = "SELECT COUNT(id) as Tot FROM Ordine WHERE emailCompratore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint;
         $stmt = mysqli_prepare($connection, $count_purchases);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -37,37 +37,19 @@
 
         //money spent
         $money_spent = "SELECT CAST(SUM(prezzo*quantita)/100 AS DECIMAL(10,2)) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailCompratore = ? AND ".$time_constraint;
+        WHERE emailCompratore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint;
         $stmt = mysqli_prepare($connection, $money_spent);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
         $analytics["Denaro speso:"] = $row[0]["Tot"]."€";
-
-        //reviews
-        $count_reviews = "SELECT COUNT(id) as Tot FROM Recensione WHERE email = ? AND ".$time_constraint;
-        $stmt = mysqli_prepare($connection, $count_reviews);
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $analytics["Recensioni scritte:"] = $row[0]["Tot"];
-
-        //followed sellers
-        $count_followers = "SELECT COUNT(emailVenditore) as Tot FROM Follow WHERE emailCompratore = ?";
-        $stmt = mysqli_prepare($connection, $count_followers);
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $analytics["Venditori seguiti:"] = $row[0]["Tot"];
     }
     else if(getUserType() == UserType::SELLER->value){
 
         //Total revenue
         $money_earn = "SELECT COALESCE(CAST(SUM(prezzo*quantita)/100 AS DECIMAL(10,2)),0) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailVenditore = ? AND ".$time_constraint;
+        WHERE emailVenditore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint;
         $stmt = mysqli_prepare($connection, $money_earn);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -77,7 +59,7 @@
 
         //Total products sold
         $products_sell = "SELECT COALESCE(SUM(quantita),0) as Tot FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
-        WHERE emailVenditore = ? AND ".$time_constraint;
+        WHERE emailVenditore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint;
         $stmt = mysqli_prepare($connection, $products_sell);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -88,7 +70,7 @@
         //Most sold product
         $most_sold = "SELECT COALESCE(SUM(quantita),0) as Tot,Prodotto.Nome as Nome,Prodotto.id AS pId FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
         INNER JOIN Variante ON InfoOrdine.idVariante = Variante.id INNER JOIN Prodotto ON Variante.idProdotto = Prodotto.id 
-        WHERE Prodotto.emailVenditore = ? AND ".$time_constraint." GROUP BY pId ORDER BY Tot DESC";
+        WHERE Prodotto.emailVenditore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint;
         $stmt = mysqli_prepare($connection, $most_sold);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
@@ -104,7 +86,7 @@
         //Most revenue product
         $most_sold = "SELECT COALESCE(CAST(SUM(InfoOrdine.quantita*InfoOrdine.prezzo)/100 AS DECIMAL(10,2)),0) as Tot,Prodotto.Nome as Nome,Prodotto.id AS pId FROM InfoOrdine INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine
         INNER JOIN Variante ON InfoOrdine.idVariante = Variante.id INNER JOIN Prodotto ON Variante.idProdotto = Prodotto.id 
-        WHERE Prodotto.emailVenditore = ? AND ".$time_constraint." GROUP BY pId ORDER BY Tot DESC";
+        WHERE Prodotto.emailVenditore = ?  AND DATEDIFF(CURDATE(), Ordine.dataCreazione)<=".$time_constraint." GROUP BY pId ORDER BY Tot DESC";
         $stmt = mysqli_prepare($connection, $most_sold);
         mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
--- pages/api/getAnalyticsGraphs.php
+++ pages/api/getAnalyticsGraphs.php
@@ -23,23 +23,24 @@
             if($_GET["limit"]==="week"){
                 $revenue = "SELECT DATE_FORMAT(Ordine.dataCreazione, '%Y-%m-%d') AS day,
                 COALESCE(CAST(SUM(InfoOrdine.quantita*InfoOrdine.prezzo)/100 AS DECIMAL(10,2)),0) AS Tot From InfoOrdine
-                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? AND DAY(Ordine.dataCreazione) = DAY(CURDATE())
+                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? and DATEDIFF(CURDATE(), Ordine.dataCreazione) <=6
                 group by day
                 Order by day";
             }
             if($_GET["limit"]==="month"){
                 $revenue = "SELECT DATE_FORMAT(Ordine.dataCreazione, '%Y-%m') AS month,
-                WEEK(Ordine.dataCreazione, 3) - WEEK(DATE_SUB(Ordine.dataCreazione, INTERVAL DAY(Ordine.dataCreazione) - 1 DAY), 3) + 1 AS week,
+                WEEK(Ordine.dataCreazione, 3) - WEEK(DATE_SUB(Ordine.dataCreazione, INTERVAL DAY(Ordine.dataCreazione) - 1 DAY), 3) + 1 AS week_number,
+                CONCAT(DATE_FORMAT(Ordine.dataCreazione, '%Y-%m'),' settimana ',WEEK(Ordine.dataCreazione, 3) - WEEK(DATE_SUB(Ordine.dataCreazione, INTERVAL DAY(Ordine.dataCreazione) - 1 DAY), 3) + 1 ) AS week,
                 COALESCE(CAST(SUM(InfoOrdine.quantita*InfoOrdine.prezzo)/100 AS DECIMAL(10,2)),0) AS Tot From InfoOrdine
-                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? 
-                  AND YEAR(Ordine.dataCreazione) = YEAR(CURDATE()) AND MONTH(Ordine.dataCreazione) = MONTH(CURDATE())
-                    GROUP BY month, week
-                    ORDER BY month, week";
+                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? AND DATEDIFF(CURDATE(), Ordine.dataCreazione) <=30
+                    GROUP BY month, week_number
+                    ORDER BY month, week_number";
             }
             if($_GET["limit"]==="year"){
                 $revenue = "SELECT DATE_FORMAT(Ordine.dataCreazione, '%Y-%m') AS month,
                 COALESCE(CAST(SUM(InfoOrdine.quantita*InfoOrdine.prezzo)/100 AS DECIMAL(10,2)),0) AS Tot From InfoOrdine
-                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? group by month Order by month";
+                INNER JOIN Ordine ON Ordine.id = InfoOrdine.idOrdine WHERE emailVenditore=? AND DATEDIFF(CURDATE(), Ordine.dataCreazione) <=365
+                GROUP BY month ORDER BY month";
             }
         }
 
